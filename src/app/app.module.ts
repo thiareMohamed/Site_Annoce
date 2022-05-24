b4482ea1e76a3a3e0c8403a1f756148765fcec2a
thiaremohamed.mt@gmail.com <thiaremohamed.mt@gmail.com>
2022-05-24 12:21:29 +0000

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {Route, RouterModule, Routes} from "@angular/router";
+import { FooterComponent } from './footer/footer.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { AjoutAnnnonceComponent } from './ajout-annnonce/ajout-annnonce.component';
+import { ListeAnnonceComponent } from './liste-annonce/liste-annonce.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { DetailComponent } from './detail/detail.component';
+import {HttpClientModule} from "@angular/common/http";
 
+
+const routes : Routes = [
+  {path : "accueil", component : AccueilComponent},
+  {path : "add", component : AjoutAnnnonceComponent},
+  {path : "detail", component : DetailComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    AccueilComponent,
+    AjoutAnnnonceComponent,
+    ListeAnnonceComponent,
+    DetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
